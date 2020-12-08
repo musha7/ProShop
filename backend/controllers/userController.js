1e7096c73472d254b29edb9852c7053b39773765
Michael Shlezinger <michaelshl7@gmail.com>
2020-12-08 17:51:06 +0200

    Updated cartscreen and header. Added login,rigister and profile pages

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,7 +62,6 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route       GET /api/users/profile
 // @access      Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    console.log(req.user._id);
     const user = await User.findById(req.user._id)
 
     if (user) {
@@ -78,4 +77,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile, registerUser }
+// @description Update user profile
+// @route       PUT /api/users/profile
+// @access      Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        })
+    } else {
+        res.status(404)
+        throw new Error('User Not Found')
+    }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
