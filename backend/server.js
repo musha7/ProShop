b0b3ae3e3a3dc64f8f89f6bbd217c8475400e9be
Michael Shlezinger <michaelshl7@gmail.com>
2020-12-16 13:11:37 +0200

    lastthings added for preparation for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+
 
 app.use('/api/products', productRoutes)
 
@@ -37,6 +35,17 @@ app.use('/api/upload', uploadRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
+else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
 
 app.use(notFound)
 
