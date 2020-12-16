3b7c1193f522ee52abfa683f372dda7c369086d2
Michael Shlezinger <michaelshl7@gmail.com>
2020-12-16 13:30:11 +0200

    homescreen fix

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -34,7 +34,7 @@ const HomeScreen = ({ match }) => {
                 <>
                     <Row>
                         {products.map(product => (
-                            <Col key={product._id} sm={12} md={6} lg={4} xl={2}>
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                                 <Product product={product} />
                             </Col>
                         ))}
